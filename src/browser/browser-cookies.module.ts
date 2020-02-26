a8769019f340f6e47b59f598ebb6b747b85fb464
Anton Barada <themrbarada@gmail.com>
2020-02-26 16:18:24 +0200

    fix build

--- src/browser/browser-cookies.module.ts
+++ src/browser/browser-cookies.module.ts
@@ -1,19 +1,21 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-
-import { CookiesOptions } from '../cookies-options';
-import { COOKIES_OPTIONS, CookiesOptionsService } from '../cookies-options.service';
-import { CookiesService } from '../cookies.service';
-import { BrowserCookiesService } from './browser-cookies.service';
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { CookiesOptions } from "../cookies-options";
+import {
+  CookiesOptionsService,
+  COOKIES_OPTIONS
+} from "../cookies-options.service";
+import { CookiesService } from "../cookies.service";
+import { BrowserCookiesService } from "./browser-cookies.service";
 
 @NgModule()
 export class BrowserCookiesModule {
-  static forRoot(options: CookiesOptions = {}): ModuleWithProviders<BrowserCookiesModule> {
+  static forRoot(options: CookiesOptions = {}): ModuleWithProviders {
     return {
       ngModule: BrowserCookiesModule,
       providers: [
-        {provide: COOKIES_OPTIONS, useValue: options},
+        { provide: COOKIES_OPTIONS, useValue: options },
         CookiesOptionsService,
-        {provide: CookiesService, useClass: BrowserCookiesService}
+        { provide: CookiesService, useClass: BrowserCookiesService }
       ]
     };
   }
--- src/server/server-cookies.module.ts
+++ src/server/server-cookies.module.ts
@@ -1,19 +1,21 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-
-import { CookiesOptions } from '../cookies-options';
-import { COOKIES_OPTIONS, CookiesOptionsService } from '../cookies-options.service';
-import { CookiesService } from '../cookies.service';
-import { ServerCookiesService } from './server-cookies.service';
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { CookiesOptions } from "../cookies-options";
+import {
+  CookiesOptionsService,
+  COOKIES_OPTIONS
+} from "../cookies-options.service";
+import { CookiesService } from "../cookies.service";
+import { ServerCookiesService } from "./server-cookies.service";
 
 @NgModule()
 export class ServerCookiesModule {
-  static forRoot(options: CookiesOptions = {}): ModuleWithProviders<ServerCookiesModule> {
+  static forRoot(options: CookiesOptions = {}): ModuleWithProviders {
     return {
       ngModule: ServerCookiesModule,
       providers: [
-        {provide: COOKIES_OPTIONS, useValue: options},
+        { provide: COOKIES_OPTIONS, useValue: options },
         CookiesOptionsService,
-        {provide: CookiesService, useClass: ServerCookiesService}
+        { provide: CookiesService, useClass: ServerCookiesService }
       ]
     };
   }
